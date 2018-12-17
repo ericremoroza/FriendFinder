3bdd676f65541a6b65c299b812b02cb2e9bb8945
ericremoroza <eric.remoroza@outlook.com>
2018-12-17 12:36:42 -0500

    Changed questions and added Bootstrap link

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,39 +1,53 @@
 var path = require("path");
 
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function (app) {
-    app.get("api/friends", function (require, result) {
-        result.json(friends);
+    app.get("api/friends", function (req, res) {
+        res.json(friends);
     });
 
-    app.post("api/friends", function (require, result) {
-        var userInput = require.body;
+    app.post("api/friends", function (req, res) {
+        
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
 
-        for (var i = 0; i < userInput.scores.length; i++) {
-            userInput.scores[i] = parseInt(userInput.scores[i]);
-        }
+        console.log(req.body);
 
-        var friendIndex = 0;
-        var minDiff = 1000;
+        // Parse the result of user's survey POST
+        var userData = req.body;
+        var userScores = userData.scores;
 
-        //Start with zero difference and compare scores, add that difference to the totalDiff
-        for (var i = 0; friends.length; i++) {
-            var totalDiff = 0;
-            for (var j = 0; j < friends[i].scores.length; j++) {
-                var difference = Math.abs(userInput.scores[j] - friends[i].scores[j]);
-                totalDiff += difference;
+        console.log(userScores);
+
+        // Calculates difference between user's scores and scores of each user in database
+        var totalDifference = 0;
+
+        // Loop all friend possibilities in database
+        for (var i = 0; i <friends.length; i++) {
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            // Next, loop friend scores
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+                // Calculate difference between scores and sum that into the total difference
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt((friends[i].scores[j])));
+
+                // If the sum of the differences < differences of current "best match"
+                if (totalDifference <= bestMatch.friendDifference) {
+
+                    // Reset bestMatch to be new friend
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
             }
-
-            if (totalDiff < minDiff) {
-                friendIndex = i;
-                minDiff = totalDiff;
-            }
         }
-
-        friends.push(userInput);
-
-        res.json(friends[friendIndex]);
-
+        friends.push(userData);
+        res.json(bestMatch);
     });
 }
+
--- server.js
+++ server.js
@@ -9,15 +9,18 @@ var app = express();
 //Initial port for listener
 var PORT = process.env.PORT || 8080;
 
+var jsonParser = bodyParser.json();
 
-//Express app parses data
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var urlEncodedParser = bodyParser.urlencoded({ extended: false})
 
-//Router
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+app.use(bodyParser.json({type: "application/*+json"}))
 
+app.use(bodyParser.raw({ type: "application/vnd.custom-type"}))
+
+app.use(bodyParser.text({type: "text/html" }))
+
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 //Listener to start server
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
